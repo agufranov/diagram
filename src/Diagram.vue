3f97ddb3dc7eeff9229bf75f28f6ba9590e261c3
agufranov <al.gufranov@gmail.com>
2025-11-07 21:31:33 +0300

    advanced

--- src/Diagram.vue
+++ src/Diagram.vue
@@ -1,82 +1,128 @@
 <script setup lang="ts">
-import { computed, ref, useTemplateRef, watch } from 'vue'
+import { computed, ref, useTemplateRef } from 'vue'
 
-const maxZoom = 20
+const maxZoom = 40
 const minZoom = 1
 const baseWidth = 100
 const baseZoom = 10
 const zoomFactor = 0.3
+const inertiaFactor = 0.03
+const inertiaMax = 100
 
-const data = [
-  [-40, -20],
-  [-10, 20],
-  [40, 45],
-]
+const rnd = (x: number) => Math.random() * x
+const range = (n: number) => [...Array(n).keys()]
 
-const center = ref(0)
-const width = ref(baseWidth)
+const data: [number, number][] = [[-1000, -950]]
+for (let i = 0; i < 100000; i++) {
+  const end = data[data.length - 1]![1]
+  data.push([end + rnd(100), end + rnd(100) + rnd(200)])
+}
+// const data: [number, number][] = [
+//   [-40, 0],
+//   [10, 20],
+//   [30, 40],
+// ]
+
 const zoom = ref(baseZoom)
+const from = ref(-50)
+const to = ref(from.value + baseWidth)
+const width = computed(() => to.value - from.value)
+const zoomCoef = computed(() => Math.exp((zoom.value - baseZoom) * zoomFactor))
+const inertiaDistance = computed(
+  () => Math.min(inertia.value ?? 0, inertiaMax) * inertiaFactor * zoomCoef.value,
+)
 
-const from = computed(() => center.value - width.value / 2)
-const to = computed(() => center.value + width.value / 2)
-
-const dragStart = ref<number>(null)
-const dragCenter = ref<number>(null)
+const dragStart = ref<number | null>(null)
+const dragFrom = ref<number | null>(null)
+const lastDragX = ref<number | null>(null)
+const inertia = ref<number | null>(null)
 
 const elRef = useTemplateRef<HTMLDivElement>('ref')
 
-watch(zoom, () => {
-  width.value = baseWidth * Math.exp((zoom.value - baseZoom) * zoomFactor)
-})
+const visibleData = computed(() =>
+  data.filter(
+    (event) =>
+      event[1] >=
+        from.value - (width.value * Math.exp(zoomFactor)) / 2 - Math.abs(inertiaDistance.value) &&
+      event[0] <=
+        to.value + (width.value * Math.exp(zoomFactor)) / 2 + Math.abs(inertiaDistance.value),
+  ),
+)
 
 const toScreen = (x: number) => {
   return ((elRef.value?.clientWidth ?? 0) * (x - from.value)) / width.value
 }
 
 const fromScreen = (x: number) => {
-  return from.value + (width.value * x) / elRef.value.clientWidth
+  return from.value + ((to.value - from.value) * x) / (elRef.value?.clientWidth ?? 0)
 }
 
 Object.assign(window, { toScreen, fromScreen })
 
-const handleScroll = (e: WheelEvent) => {
-  zoom.value = Math.min(maxZoom, Math.max(minZoom, zoom.value - Math.sign(e.deltaY)))
+const handleWheel = (e: WheelEvent) => {
+  zoom.value = Math.min(maxZoom, Math.max(minZoom, zoom.value + Math.sign(e.deltaY)))
+
+  const zoomCenter = fromScreen(e.clientX)
+  const k = Math.exp(Math.sign(e.deltaY) * zoomFactor)
+
+  from.value = zoomCenter + (from.value - zoomCenter) * k
+  to.value = zoomCenter + (to.value - zoomCenter) * k
 }
 
-const handleMouseDown = (e: MouseEvent) => {
+const handlePointerDown = (e: PointerEvent) => {
   dragStart.value = e.clientX
-  dragCenter.value = center.value
+  dragFrom.value = from.value
+  elRef.value?.setPointerCapture(e.pointerId)
+  lastDragX.value = e.clientX
 }
 
-const handleMouseMove = (e: MouseEvent) => {
-  if (dragStart.value !== null && dragCenter.value !== null) {
-    center.value = fromScreen(toScreen(dragCenter.value) - (e.clientX - dragStart.value))
+const handlePointerMove = (e: PointerEvent) => {
+  if (dragStart.value !== null && dragFrom.value !== null) {
+    from.value = fromScreen(toScreen(dragFrom.value) - (e.clientX - dragStart.value))
+    to.value = from.value + baseWidth * zoomCoef.value
+    if (lastDragX.value !== null) {
+      inertia.value = e.clientX - lastDragX.value
+    }
+    lastDragX.value = e.clientX
   }
 }
 
-const handleMouseUp = () => {
+const handlePointerUp = (e: PointerEvent) => {
+  if (dragStart.value === e.clientX) {
+    // inertia.value = 0
+  }
   dragStart.value = null
-  dragCenter.value = null
+  dragFrom.value = null
+  elRef.value?.releasePointerCapture(e.pointerId)
+  from.value -= inertiaDistance.value
+  to.value -= inertiaDistance.value
 }
 </script>
 
 <template>
   <div
     :class="$style.container"
-    @wheel="handleScroll"
-    @mousedown="handleMouseDown"
-    @mousemove="handleMouseMove"
-    @mouseup="handleMouseUp"
+    @wheel="handleWheel"
+    @pointerdown="handlePointerDown"
+    @pointermove="handlePointerMove"
+    @pointerup="handlePointerUp"
+    :style="{
+      cursor: dragStart ? 'grabbing' : 'grab',
+    }"
     ref="ref"
   >
     {{ zoom }}
     {{ width.toFixed(0) }}
-    {{ from }} {{ to }}
+    {{ from.toFixed(1) }} {{ to.toFixed(1) }}
     <div
-      v-for="[from, to] in data"
+      v-for="[from, to] in visibleData"
       :key="from"
       :class="$style.event"
-      :style="{ left: `${toScreen(from)}px`, width: `${toScreen(to) - toScreen(from)}px` }"
+      :style="{
+        left: `${toScreen(from)}px`,
+        width: `${toScreen(to) - toScreen(from)}px`,
+        transition: dragStart !== null ? 'none' : 'all ease 0.3s',
+      }"
     ></div>
   </div>
 </template>
